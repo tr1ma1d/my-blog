71fedbc2796151c524c95327a892d4ae7dac3349
Nikita <musatovla3xnikita@gmail.com>
2024-05-23 22:52:12 +0300

    create header

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import Header from './Components/Header'
 import Content from './Components/Content';
-import Footer from './Components/Footer'
+import Footer from './Components/Footer';
+import { BrowserRouter as Route } from 'react-router-dom';
 function App() {
   return (
     <div>
-  
+      <Route>
+        <Header/>
+        <Content/>
+        <Footer/>
+      </Route>
+      
     </div>
 
   )
 }
 
 export default App;
+
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
-
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './css-style/header-style.css';
 
 const Header = () => {
-    return(
-        <div>Привет мир</div>
+    return (
+        <header className="top">
+            <div>
+                <div className="top-logo">
+                    <span className="top-logo__text">
+                        Nikita
+                    </span>
+                </div>
+                <div className="top-menu">
+                    <ul>
+                        <li>
+                            <Link className="top-menu__link" to="#">Home</Link>
+                        </li>
+                        <li>
+                            <Link className="top-menu__link" to="#">App</Link>
+                        </li>
+                        <li>
+                            <Link className="top-menu__link" to="#">Wallpaper</Link>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </header>
     );
 }
+
 export default Header;
